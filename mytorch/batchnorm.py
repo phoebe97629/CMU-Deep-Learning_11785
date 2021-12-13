ef605e916b67768a5dd68714700c880db2bcf570
Phoebe Li <74167244+phoebe97629@users.noreply.github.com>
2021-12-13 16:08:59 -0500

    Add files via upload

---
+++ mytorch/batchnorm.py
@@ -0,0 +1,74 @@
+# Do not import any additional 3rd party external libraries as they will not
+# be available to AutoLab and are not needed (or allowed)
+
+import numpy as np
+
+class BatchNorm(object):
+
+    def __init__(self, in_feature, alpha=0.9):
+
+        # You shouldn't need to edit anything in init
+
+        self.alpha = alpha
+        self.eps = 1e-8
+        self.x = None
+        self.norm = None
+        self.out = None
+
+        # The following attributes will be tested
+        self.var = np.ones((1, in_feature))
+        self.mean = np.zeros((1, in_feature))
+
+        self.gamma = np.ones((1, in_feature))
+        self.dgamma = np.zeros((1, in_feature))
+
+        self.beta = np.zeros((1, in_feature))
+        self.dbeta = np.zeros((1, in_feature))
+
+        # inference parameters
+        self.running_mean = np.zeros((1, in_feature))
+        self.running_var = np.ones((1, in_feature))
+
+    def __call__(self, x, eval=False):
+        return self.forward(x, eval)
+
+    def forward(self, x, eval=False):
+        """
+        Argument:
+            x (np.array): (batch_size, in_feature)
+            eval (bool): inference status
+
+        Return:
+            out (np.array): (batch_size, in_feature)
+
+        NOTE: The eval parameter is to indicate whether we are in the 
+        training phase of the problem or are we in the inference phase.
+        So see what values you need to recompute when eval is True.
+        """
+
+        # if eval:
+        #    # ???
+
+        self.x = x
+
+        # self.mean = # ???
+        # self.var = # ???
+        # self.norm = # ???
+        # self.out = # ???
+
+        # Update running batch statistics
+        # self.running_mean = # ???
+        # self.running_var = # ???
+
+        raise NotImplemented
+
+
+    def backward(self, delta):
+        """
+        Argument:
+            delta (np.array): (batch size, in feature)
+        Return:
+            out (np.array): (batch size, in feature)
+        """
+
+        raise NotImplemented
